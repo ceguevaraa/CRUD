ef3d9e2363309cbf793dcb8d6843102b54bb751b
Cesar Guevara <cesar.guevara@advantagesolutions.net>
2023-03-23 09:36:51 -0500

    Ground work.

---
+++ Phonebook/Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace Phonebook.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
