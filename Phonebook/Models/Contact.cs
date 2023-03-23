f2da01f1587eb98628df6630df5da144b3a1899f
Cesar Guevara <cesar.guevara@advantagesolutions.net>
2023-03-23 12:09:37 -0500

    Ground work for backend

--- Phonebook/Models/Contact.cs
+++ Phonebook/Models/Contact.cs
@@ -2,6 +2,7 @@
 {
     public class Contact
     {
+        public int Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
